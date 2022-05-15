a53683da33cb3e1bc1522b8787fb528f7f7a7e91
eugebrest <eugeniabrest@hotmail.com>
2022-05-15 19:27:33 -0300

    Agregué rutas

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const publicPath = path.join(__dirname, "./public");
 const homePath = path.join(__dirname, "./views/products.html");
 const productPath = path.join(__dirname, "./views/products.html");
 const productDetailPath = path.join(__dirname, "./views/productDetail.html");
+const registerPath = path.join(__dirname, "./views/register.html");
+const loginPath = path.join(__dirname, "./views/login.html");
+const cart1Path = path.join(__dirname, "./views/cart1.html");
+const cart2Path = path.join(__dirname, "./views/cart2.html");
+const cart3Path = path.join(__dirname, "./views/cart3.html");
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
@@ -25,4 +30,24 @@ res.sendFile(productPath);
 
 app.get("/productDetail", (req, res) => {
     res.sendFile(productDetailPath);
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(registerPath);
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(loginPath);
+});
+
+app.get("/cart1", (req, res) => {
+    res.sendFile(cart1Path);
+});
+
+app.get("/cart2", (req, res) => {
+    res.sendFile(cart2Path);
+});
+
+app.get("/cart3", (req, res) => {
+    res.sendFile(cart3Path);
 });
