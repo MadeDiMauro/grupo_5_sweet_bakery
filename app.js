d35769ff73aa42a41948317f862c775dcf88d1f0
Madeleine Di Mauro <damaris.dmauro@gmail.com>
2022-05-16 00:07:43 -0300

    Creación de footer y header - versión desktop

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const loginPath = path.join(__dirname, "./views/login.html");
 const cart1Path = path.join(__dirname, "./views/cart1.html");
 const cart2Path = path.join(__dirname, "./views/cart2.html");
 const cart3Path = path.join(__dirname, "./views/cart3.html");
+
+const basePath = path.join(__dirname, "./views/plantillas/base.html");
+const headerPath = path.join(__dirname, "./views/plantillas/header.html");
+const footerPath = path.join(__dirname, "./views/plantillas/footer.html");
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
@@ -50,4 +55,16 @@ app.get("/cart2", (req, res) => {
 
 app.get("/cart3", (req, res) => {
     res.sendFile(cart3Path);
+});
+
+app.get("/base", (req, res) => {
+    res.sendFile(basePath)
+})
+
+app.get("/header", (req, res) => {
+	res.sendFile(headerPath);
+});
+
+app.get("/footer", (req, res) => {
+	res.sendFile(footerPath);
 });
