5e172733fc2a4b669232ad6eb0ca0ac0e23d4264
eugebrest <eugeniabrest@hotmail.com>
2022-06-05 22:24:49 -0300

    cambié idioma productsController

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,7 +1,7 @@
 const fs= require ("fs");
 const path=require ("path");
 
-const productosdbPath=path.join (__dirname, "../database/productos.json");
+const productsdbPath=path.join (__dirname, "../database/products.json");
 
 const readJsonFile= (path)=> {
     const data=fs.readFileSync (path, "utf-8");
@@ -9,21 +9,21 @@ const readJsonFile= (path)=> {
     return dataParsed;
 }
 
-const listaProductos=readJasonFile (productosdbPath);
+const productsList=readJasonFile (productsdbPath);
 
 const productsController = {
 
     products: (req,res)=>{
-        return res.render ("products", {listaProductos:listaProductos})
+        return res.render ("products", {productsList:productsList})
     },
     productDetail: (req,res)=> {
-        return res.render ('productDetail', {listaProductos:listaProductos}) // validacion y sin objeto completo
+        return res.render ('productDetail', {productsList:productsList}) // validacion y sin objeto completo
     },
     create: (req, res) => {
         return res.render ('create')
     },
     edit: (req, res) => {
-        return res.render ('edit', {listaProductos:listaProductos}) //validacion y son objeto (en el ejs, entre llaves solo comparto el item filtrado, se filtra aantes del render)
+        return res.render ('edit', {productsList:productsList}) //validacion y son objeto (en el ejs, entre llaves solo comparto el item filtrado, se filtra aantes del render)
     },
 
 }
