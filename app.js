25608f645dd756fe689c0f91d92bf5ffad7037ba
eugebrest <eugeniabrest@hotmail.com>
2022-06-09 19:59:11 -0300

    cambios

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.listen(PORT, () => {
 });
 
 app.set ("view engine", "ejs");
-app.set ("views", path.join (__dirname, "views"));
+app.set ("views", path.join (__dirname, "./views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
