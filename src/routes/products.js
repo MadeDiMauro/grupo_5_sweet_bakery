134de7923f8c60e919d9c08ab64aa58f6edf7802
MarianoDH1 <iniguezmar@gmail.com>
2022-06-09 21:08:31 -0300

    controller

---
+++ src/routes/products.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const productsController = require("../src/controllers/productsController.js")
+
+router.get('/list/:category?', productsController.list);
+router.get('/detail/:id', productsController.detail);
+router.get('/create', productsController.create);
+router.get('/edit/:id', productsController.edit);
+
+module.exports = router;
