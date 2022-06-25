0c48e23a9d07e0f2b8caa36564efa91ff6fda5bd
eugebrest <eugeniabrest@hotmail>
2022-06-25 19:12:44 -0300

    plantilla about

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,3 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+
+const usersdbPath = path.join(__dirname, "../database/users.json");
+
+const readJsonFile = (path) => {
+    const dataUsers = fs.readFileSync(path, { encoding: 'utf-8' });
+    const dataUsersParsed = JSON.parse(dataUsers);
+    return dataUsersParsed;
+}
+
+const usersList=readJsonFile(usersdbPath); /* no me lo lee*/
+
 const usersController = {
   login: (req, res) => {
     return res.render('users/login')
