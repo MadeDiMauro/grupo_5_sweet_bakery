0853bce5b05536426c366a893dc7fc01ecef1991
eugebrest <eugeniabrest@hotmail>
2022-07-09 16:17:40 -0300

    modifiqué userLoggedMiddleware

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -2,7 +2,7 @@ function userLoggedMiddleware (req,res,next) {
     
     if (req.session && req.session.userLogged) {
         res.locals.isLogged=false;
-        res.locals.isLogged=req.session.userLogged;  /*paso lo que tengo en session a locals*/
+        res.locals.isLogged=req.session.userLogged;  /*paso lo que tengo en session a locals para después usarlo en la vista de nabvaruser*/
     }
     next ();
 }
