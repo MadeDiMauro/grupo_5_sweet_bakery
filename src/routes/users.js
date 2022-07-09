840dc15f7c41080199a3e6af01c1294c7e390acc
MarianoDH1 <iniguezmar@gmail.com>
2022-07-08 22:43:48 -0300

    modificacion euge

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,6 +4,7 @@ const usersController = require("../controllers/usersController.js");
 
 router.get('/login', usersController.login);
 router.get('/register', usersController.register);
+router.post('/register', usersController.processRegister);
 //router.get('/profile/:id', usersController.profile);
 
 module.exports = router;
