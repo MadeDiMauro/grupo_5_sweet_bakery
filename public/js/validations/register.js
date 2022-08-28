38c171337223016ff906dcad4a1f317d59c99af8
eugebrest <eugeniabrest@hotmail>
2022-08-28 20:48:30 -0300

    modificación de register front validation

--- public/js/validations/register.js
+++ public/js/validations/register.js
@@ -1,52 +1,60 @@
 
-    /*let listadoErrores = document.querySelector(".errores");*/
 window.addEventListener ('load', function (){
-    let form= document.querySelector ('form')
-    let arrayInputs = Array.from(form.elements);
-    let listErrors = document.querySelector(".errors");
+    let form= document.querySelector ('.form')
+    let inputName=document.querySelector ('name')
+    let inputEmail=document.getElementById ('email')
+    let inputPassword=document.getElementById ('password')
+    let inputAvatar=document.getElementById ('avatar')
+
+    const arrayInputs =  [inputName, inputEmail, inputPassword, inputAvatar];
+  
 
     form.addEventListener ('submit', function (e) {
         e.preventDefault();
-
-    /*let errors = 0;
-    arrayInputs.forEach(function(input) {
-    if(input.value == ""){
-        errors++;
-        listErrors.innerHTML += "<li>El campo "+ input.dataset.name +" no puede estar vacío</li>";
-    } 
-})
+        
+        arrayInputs.forEach(function(input) {
+        input.addEventListener("blur", function() {
+                    if(inputName.value === ""){
+                    input.nextElementSibling.nextElementSibling.classList.add("is-invalid");
+                    input.classList.add("is-invalid")
+                    input.classList.remove("is-valid")
+                    input.nextElementSibling.nextElementSibling.innerHTML = "Debes ingresar un nombre";
+                } else if  (inputName.value.lenght>2) {
+                    input. nextElementSibling.nextElementSibling.innerHTML = 'Debes ingresar al menos dos caracteres'
+                    input.classList.add("is-invalid")
+                    input.classList.remove("is-valid")
+                }
+            })
+        })
     })
-})*/
-    let errors=[]
-
-    let inputName=document.querySelector ('#name')
+})
+/*
     if (inputName.value=""){
         errors.push ('Debes ingresar un nombre');
     } else if (inputName.value.lenght>2) {
         errors.push ('Debes ingresar al menos dos caracteres')
     }
 
-    /*let expression = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    let inputEmail=document.getElementById ('email')
+    let expression = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    
     if (inputEmail.value=""){
         errors.push ('Debes ingresar un email');
     } else if ( !expression.test(email) ){
         errors.push ('Debes ingresar un email válido')
     }
 
-    let inputPassword=document.getElementById ('password')
+    
     if (inputPassword.value=""){
-        errors.push ('Debes ingresar una contraseña')
+    errors.push ('Debes ingresar una contraseña')
     } else if (inputPassword.value>8) {
         errors.push ('Debes ingresar al menos ocho caracteres')
     }
 
-    let inputAvatar=document.getElementById ('avatar')
     let acceptedExtensions = ['.jpg', '.png', '.gif'];
     if (inputAvatar.value!==acceptedExtensions) {
         errors.push ('Debes ingresar' + acceptedExtensions)
     }
-*/
+
     if (errors.length>0) {
         e.preventDefault();
         let ulErrors=document.querySelector ('div.errors ul')
@@ -55,7 +63,4 @@ window.addEventListener ('load', function (){
         ulErrors.innerHTML+= "<li>" + errors [i] + "</li>"
 
     }
-}
-    })
-})
-
+}*/
