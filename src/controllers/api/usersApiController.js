5f9dae5718ca2d6a007a3f29f04b8a7004934aca
eugebrest <eugeniabrest@hotmail>
2022-09-12 22:03:58 -0300

    armando usersApiController

--- src/controllers/api/usersApiController.js
+++ src/controllers/api/usersApiController.js
@@ -5,12 +5,43 @@ const Op = Sequelize.Op;
 
 const usersApiController = {
     usersList: async (req, res) => {
-        //Hacer consulta a la BD
-      return res.json("Hola :) soy la api");
-    },
+        db.user.findAll ()
+        .then (users => {
+          return res.status (200).json ({
+            meta: {
+              code:res.statusCode,
+              total:users.length,
+            },
+            user: {
+            /*id:,
+              name:,
+              email:,*/
+
+            }
+
+            })
+          })
+        
+      }, 
+
+
+      /*Hacer consulta a la BD
+      //return res.json("Hola :) soy la api");*/
+
     userId: async (req, res) => {
-        //Hacer consulta a la BD
-      return res.json("Hola :) soy la api");
+      db.user.findByPk(req.params.id)
+      .then(user => {
+       return res.status(200).json({
+                  meta: {
+                      code: res.statusCode,
+                      url: req.protocol + "://" + req.get("host") + req.originalUrl
+                  },
+                  data: user
+              })
+          });
+        
+      /*Hacer consulta a la BD
+      return res.json("Hola :) soy la api");*/
     }
 };
 
