4d9973ef11f2f636d2eff6e762351db954771a8a
eugebrest <eugeniabrest@hotmail>
2022-09-12 23:40:37 -0300

    armando productsApiController y usersApiController

--- src/controllers/api/usersApiController.js
+++ src/controllers/api/usersApiController.js
@@ -7,28 +7,26 @@ const usersApiController = {
     usersList: async (req, res) => {
         db.user.findAll ()
         .then (users => {
-          return res.status (200).json ({
+          let array= [];
+          users.forEach ((users)=> {
+          array.push ({
+            id:users.id,
+            name:users.name,
+            email:users.email,
+            avatar: "/images/avatars/" + users.avatar
+          })
+          }) 
+          return res.status(200).json ({
             meta: {
-              code:res.statusCode,
+              code:res.statusCode, // status: 200, 
               total:users.length,
-            },
-            user: {
-            /*id:,
-              name:,
-              email:,*/
-
+              data: array
             }
-
             })
           })
         
       }, 
-
-
-      /*Hacer consulta a la BD
-      //return res.json("Hola :) soy la api");*/
-
-    userId: async (req, res) => {
+   userId: async (req, res) => {
       db.user.findByPk(req.params.id)
       .then(user => {
        return res.status(200).json({
@@ -39,10 +37,7 @@ const usersApiController = {
                   data: user
               })
           });
-        
-      /*Hacer consulta a la BD
-      return res.json("Hola :) soy la api");*/
-    }
+          }
 };
 
 module.exports = usersApiController;
