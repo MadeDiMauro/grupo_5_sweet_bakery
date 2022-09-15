88e4fd2b5b4b5e7f1d493c66a3698b6712397d71
Aldana-Lorenzo <aldana.lorenzo@gmail.com>
2022-09-14 23:34:25 -0300

    api-products

--- src/controllers/api/productsApiController.js
+++ src/controllers/api/productsApiController.js
@@ -3,93 +3,76 @@ const db = require("../../database/models");
 const Op = Sequelize.Op;
 
 const productsApiController = {
-    productsList: async (req, res) => {
-      let products = db.products.findAll();
-      let categories = db.products_categories.findAll(); 
+  productsList: async (req, res) => {
+    let products = await db.products.findAll({
+      include: [{
+        model: db.products_categories,
+        as: 'products_categories',
+        attributes: []
+      }],
+      attributes: [
+        [Sequelize.col('products_categories.type'), "categoria"],
+        [Sequelize.fn("COUNT", Sequelize.col("category_id")), "cantidad"]
+      ],
+      group: "category_id",
+    });
 
-      
-      Promise.all([products, categories])
-         .then ((products=> {
-          let array= [];
-          products[0].forEach ((products)=> {
-          array.push ({
-            id:products.id,
-            })
-            })
-            return res.status (200).json ({
-            meta: {
-              code:res.statusCode, // status: 200, 
-              total:products.length,
-            },
-          })
-        }))
-        /*.then (categories => {
-         return res.status (200).json ({
+    let productsList = await db.products.findAll({
+      include: [{
+        model: db.products_categories,
+        as: 'products_categories',
+      }],
+      attributes: [
+        "id", "name", "price", "description",
+        [Sequelize.fn('CONCAT', "http://localhost:3000/products/detail/", Sequelize.col('products.id')), 'url_detail'],
+      ]
+    })
+    return res.status(200).json({
+      meta: {
+        code: res.statusCode, // status: 200, 
+        count: productsList.length,
+        countByCategory: products,
+        products: productsList
+      }
+    });
+  },
+  productId: async (req, res) => {
+    await db.products.findByPk(req.params.id)
+      .then(product => {
+        return res.status(200).json({
           meta: {
-          code:res.statusCode,
-          categories: categories.length,
-           },
+            code: res.statusCode,
+            id: product.id,
+            name: product.name,
+            price: product.price,
+            description: product.description,
+            //url: req.protocol + "://" + req.get("host") + req.originalUrl
+          }
         })
-         
-     })
-       /* db.products_categories.findAll()
-        .then (products=> {
-            return res.status(200).json({
-            meta: {
-                code: res.statusCode,
-                url: req.protocol + "://" + req.get("host") + req.originalUrl,
-                data: products
-            },
-           })
-          })
-      /*countByCategory → objeto literal con una propiedad por categoría
-      con el total de productos.
-      ■ products → array con la colección de productos, cada uno con:
-      ● id
-      ● name
-      ● description
-      ● un array con principal relación de uno a muchos (ej:
-      categories).
-      ● detail → URL para obtener el detalle.*/
-      
-      
-    },
-    productId: async (req, res) => {
-     await db.products.findByPk(req.params.id)
-      .then(product => {          
-       return res.status(200).json({
-            meta: {
-              code: res.statusCode,
-              id: product.id,
-              name: product.name,
-              price: product.price,
-              description: product.description,
-              //url: req.protocol + "://" + req.get("host") + req.originalUrl
-            }
-          })
+      })
+      .then(images => {
+        db.images.findAll({
+          where: {
+            product_id
+          },
+          include: [{
+            model: db.images,
+            as: 'images'
+          }]
         })
-        .then (images => {
-          db.images.findAll({
-            where: {
-              product_id
-            },
-            include: [{
-                model: db.images,
-                as: 'images'
-            }]
-          })}
-        )
-          
-  
-   /*   ● api/products/:id
-○ Deberá devolver un objeto literal con la siguiente estructura:
-■ una propiedad por cada campo en base.
-■ un array por cada relación de uno a muchos (categories, colors,
-sizes, etc).
-■ Una URL para la imagen del producto (para mostrar la imagen).
-        //Hacer consulta a la BD
-      return res.json("Hola :) soy la api");*/
-    }
+      }
+      )
+
+
+    /*   ● api/products/:id
+ ○ Deberá devolver un objeto literal con la siguiente estructura:
+ ■ una propiedad por cada campo en base.
+ ■ un array por cada relación de uno a muchos (categories, colors,
+ sizes, etc).
+ ■ Una URL para la imagen del producto (para mostrar la imagen).
+         //Hacer consulta a la BD
+       return res.json("Hola :) soy la api");*/
+  }
 };
 
 module.exports = productsApiController;
